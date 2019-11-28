acb26cf8ec6283b0a223f516f1036eb667ac213e
Tobias Lindberg <tobias.ehlert@gmail.com>
2019-11-29 00:02:09 +0100

    adjusting some things..

--- deploy-githubpages.sh
+++ deploy-githubpages.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub Pages (tobiasehlert.github.io)...\033[0m\n"
 
 # Build the project.
-hugo-extended --minify --gc --baseURL=https://tobiasehlert.github.io/
+hugo-extended --minify --gc --baseURL https://tobiasehlert.github.io/ --enableGitInfo --environment production --destination tobiasehlert.github.io
 
 # Go To Public folder
 cd tobiasehlert.github.io
